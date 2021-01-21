6c81081107c4b548466ea28ffeb6e91953832ad5
Luke Irvin <LukeGithub@outlook.com>
2021-01-20 19:09:15 -0700

    initial check-in

---
+++ SparkfishAssessment.Utility/Listifier/Listify.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparkfishAssessment.Utility.Listifier
+{
+    public class Listify : IList<int>
+    {
+        private readonly IEnumerable<int> sequence;
+        private readonly int start;
+        private readonly int end;
+
+
+        public Listify(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+            sequence = Enumerable.Range(start, end);
+        }
+
+        public int this[int index] { get
+            {
+                if (index > (end - start))
+                    throw new IndexOutOfRangeException();
+
+                return sequence.Where((x, i) => i == index).Single();
+            } set => throw new NotImplementedException(); }
+
+        public int Count => end - start;
+
+        public bool IsReadOnly => true;
+
+        public void Add(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(int item)
+        {
+            if (item >= start && item <= end)
+                return true;
+
+            return false;
+        }
+
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(int item)
+        {
+            if (item >= start && item <= end)
+                return item - start;
+
+            return -1;
+        }
+
+        public void Insert(int index, int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SparkfishAssessment/Controllers/ListifyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SparkfishAssessment.Utility.Listifier;
+
+namespace SparkfishAssessment.Controllers
+{
+
+    [ApiController]
+    public class ListifyController : ControllerBase
+    {
+        [Route("listify/{start}/{end}/index/{idx}")]
+        [HttpGet]
+        public ActionResult<int> Get(int start, int end, int idx)
+        {
+            // Given a list of numbers ranging from 100 to 200
+            var list = new Listify(start, end);
+            // When I access index position 50
+            var val = list[idx];
+            // Then I should get back a value of 150
+            //val.Should().Equal(150);
+
+            var newIndex = list.IndexOf(150);
+
+            return val;
+        }
+    }
+}
